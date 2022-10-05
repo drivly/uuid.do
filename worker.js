315560b2152be38b501794deae0927d80bf86852
Nathan Clevenger <nateclev@gmail.com>
2022-10-05 05:04:11 -0500

    Added default quantity

--- worker.js
+++ worker.js
@@ -1,18 +1,22 @@
 export default {
-  fetch: async (req, env) => new Response(JSON.stringify({
-    api: {
-      icon: '⚡️',
-      name: 'UUID.do',
-      description: 'Unique ID Generation API',
-      url: 'https://uuid.do',
-      endpoints: {
-        generateUUID: 'https://uuid.do/api',
-        generateManyUUID: 'https://uuid.do/:quantity' 
+  fetch: async (req, env) => {
+    const path = new URL(req.url).pathname.replace('/')
+    let quantity = path == ':quantity' ? 50 : parseInt(path)
+    if (quantity == NaN) quantity = 50
+    return new Response(JSON.stringify({
+      api: {
+        icon: '⚡️',
+        name: 'UUID.do',
+        description: 'Unique ID Generation API',
+        url: 'https://uuid.do',
+        endpoints: {
+          generateUUID: 'https://uuid.do/api',
+          generateManyUUID: 'https://uuid.do/:quantity' 
+        },
+        memberOf: 'https://primitives.do',
       },
-      memberOf: 'https://primitives.do',
-    },
-    uuid: crypto.randomUUID(),
-    uuids: Array(parseInt(new URL(req.url).pathname.replace('/')) ?? 10).map(() => crypto.randomUUID()),
-    user: await env.CTX.fetch(req).then(res => res.json()).then(({user}) => user) 
-  }, null, 2)) 
+      uuid: path == 'api' ? crypto.randomUUID() : Array(quantity).map(i => crypto.randomUUID()),
+      user: await env.CTX.fetch(req).then(res => res.json()).then(({user}) => user) 
+    }, null, 2))
+  }
 }
