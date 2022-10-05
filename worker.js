cdbaa58546808218107d65c0260d5527ae21ed57
Nathan Clevenger <nateclev@gmail.com>
2022-10-05 05:07:06 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 export default {
   fetch: async (req, env) => {
-    const path = new URL(req.url).pathname.replace('/')
+    const path = new URL(req.url).pathname.replace('/','')
     let quantity = path == ':quantity' ? 50 : parseInt(path)
     if (quantity == NaN) quantity = 50
     console.log({path,quantity})
